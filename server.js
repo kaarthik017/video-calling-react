65ac132ca0b77221292e1416d6572fdb912bb581
kaarthik017 <kaarthik017@gmail.com>
2021-07-07 21:29:16 +0530

    Changes to CORS

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const http = require("http")
 const cors = require('cors')
-app.use(cors)
 const app = express()
+app.use(cors)
 const server = http.createServer(app)
 const io = require("socket.io")(server,{
     cors:{
